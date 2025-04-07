c7cbc189e3061787acda799d4c353f80d85a868e
leesj3857 <hensin12@gmail.com>
2025-04-08 01:43:25 +0900

    .env파일 commit 안되게 수정 및 Footer의 배경 색 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#1A1E1D] text-white text-center px-8 py-6">
+    <footer className="bg-[#2D3433] text-white text-center px-8 py-6">
       <p>&copy; 2025 by KOREAT. Powered and secured by Vite + React</p>
     </footer>
   );
