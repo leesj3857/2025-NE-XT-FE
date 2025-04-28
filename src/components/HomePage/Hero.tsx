1c0cb8b93b5249cc86a277d15224e3282b970924
leesj3857 <hensin12@gmail.com>
2025-04-28 18:41:09 +0900

    아이콘 추가

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -7,12 +7,15 @@ const Hero = () => {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
       className="flex flex-col md:flex-row items-start justify-between bg-[#CBCCCC] px-8 py-16 gap-8">
-      <div className="flex-1 ">
+      <div className="flex-1">
         <h2 className="text-xl md:text-[45px] mb-10">
           Discover Korea’s Best Food and Attractions
         </h2>
         <p className="mb-4 text-sm md:text-xl">
           KOREAT is a platform that introduces the best restaurants and must-see spots in Korea for foreigners.
+        </p>
+        <p className="text-sm md:text-lg">
+          The name <strong>KOREAT</strong> combines <strong>KOR</strong> (Korea), <strong>E</strong> (Eat), <strong>A</strong> (And), and <strong>T</strong> (Travel) to reflect our mission of connecting you with Korean food and cultural experiences.
         </p>
       </div>
       <img src="/HeroImage.webp" alt="Korean Meat" loading="lazy" className="w-full md:w-1/3 rounded-2xl" />
