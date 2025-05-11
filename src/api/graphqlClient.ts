89bbc47feeac42d74babd120910133ee56b98439
leesj3857 <hensin12@gmail.com>
2025-05-11 22:17:20 +0900

    API 주소 변경

--- src/api/graphqlClient.ts
+++ src/api/graphqlClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const GRAPHQL_API_URL = 'http://neeeext-env.eba-bup7qc4v.ap-northeast-2.elasticbeanstalk.com/graphql/';
 const GRAPHQL_API_URL =  import.meta.env.MODE === 'development'
 ? 'http://neeeext-env.eba-bup7qc4v.ap-northeast-2.elasticbeanstalk.com/graphql/' // 또는 개발용 실제 서버 주소
-: '/api'; // 프로덕션에서는 Netlify 프록시 경로 사용
+: '/api/'; // 프로덕션에서는 Netlify 프록시 경로 사용
 
 export const graphqlRequest = async (
   query: string,
